779ac07539fb2b2e8eb0cab6a0e4cf41f17ec52f
Dylan York <dylanyork90@outlook.com>
2021-10-02 18:04:47 -0700

    Express Node App
    This is a basic Express Node app that has an index page and my page with a navigation for my teams pages

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+// express app
+const app = express();
+
+//listen for requests 
+app.listen(3000);
+
+app.get('/', (req, res) => {
+
+    //res.send('<p>home page</p>');
+    res.sendFile('./views/index.html', { root: __dirname });
+
+});
+
+
+app.get('/Dylan', (req, res) => {
+
+    //res.send('<p>home page</p>');
+    res.sendFile('./views/dylan.html', { root: __dirname });
+
+});
+app.get('/about', (req, res) => {
+
+    //res.send('<p>about page</p>');
+
+});
